65cc95d5e1db5dfb2d3b1a21fdb69dc67f047103
abduhamidd <latipovabduhamid@gmail.com>
2021-08-20 11:52:00 +0500

    bugs

--- components/Card/Post.js
+++ components/Card/Post.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './Post.module.scss'
+import React from 'react';
+import Image from 'next/image';
+import styles from './Post.module.scss';
 
 const Post = ({
-	title = '',
-	date = '',
-	desc = '',
-	link = '',
-	thumbnail = '',
-	author = '',
-	authorImage = '',
+  title = '',
+  date = '',
+  desc = '',
+  link = '',
+  thumbnail = '',
+  author = '',
+  authorImage = '',
 }) => {
-	return (
-		<a href={link} target='_blank' rel='noopener noreferrer' alt={title}>
-			<article className={styles.card}>
-				<section className={styles.header}>
-					<Image src={thumbnail} layout='fill' objectFit='cover' />
-				</section>
-				<section className={styles.title}>
-					<h2>{title}</h2>
-				</section>
-				<section className={styles.footer}>
-					<div className={styles.authorImage}>
-						<Image src={authorImage} layout='fill' />
-					</div>
-					<div>
-						<h2 className={styles.author}>{author}</h2>
-						<p className={styles.date}>{date}</p>
-					</div>
-				</section>
-			</article>
-		</a>
-	)
-}
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer" alt={title}>
+      <article className={styles.card}>
+        <section className={styles.header}>
+          <Image src={thumbnail} layout="fill" objectFit="cover" alt="some" />
+        </section>
+        <section className={styles.title}>
+          <h2>{title}</h2>
+        </section>
+        <section className={styles.footer}>
+          <div className={styles.authorImage}>
+            <Image src={authorImage} layout="fill" alt="some" />
+          </div>
+          <div>
+            <h2 className={styles.author}>{author}</h2>
+            <p className={styles.date}>{date}</p>
+          </div>
+        </section>
+      </article>
+    </a>
+  );
+};
 
-export default Post
+export default Post;
--- components/UI/Button/CircleButton.js
+++ components/UI/Button/CircleButton.js
@@ -1,32 +1,38 @@
-import React from 'react'
-import styles from './CircleButton.module.scss'
-import { FaInstagram, FaDribbble, FaLinkedinIn, FaBehance, FaGlobe } from 'react-icons/fa'
+import React from 'react';
+import styles from './CircleButton.module.scss';
+import {
+  FaInstagram,
+  FaDribbble,
+  FaLinkedinIn,
+  FaBehance,
+  FaGlobe,
+} from 'react-icons/fa';
 
-const CircleButton = ({ types = 'default', href = '/' }) => {
-	let icon = ''
-	switch (types) {
-		case 'instagram':
-			icon = <FaInstagram />
-			break
-		case 'dribbble':
-			icon = <FaDribbble />
-			break
-		case 'behance':
-			icon = <FaBehance />
-			break
-		case 'linkedin':
-			icon = <FaLinkedinIn />
-			break
-		default:
-			icon = <FaGlobe />
-			break
-	}
-	return (
-		<a href={href} target='_blank' rel='noopener' className={styles.button}>
-			{icon}
-			<p style={{ position: 'absolute', color: 'transparent' }}>{types}</p>
-		</a>
-	)
-}
+const CircleButton = ({types = 'default', href = '/'}) => {
+  let icon = '';
+  switch (types) {
+    case 'instagram':
+      icon = <FaInstagram />;
+      break;
+    case 'dribbble':
+      icon = <FaDribbble />;
+      break;
+    case 'behance':
+      icon = <FaBehance />;
+      break;
+    case 'linkedin':
+      icon = <FaLinkedinIn />;
+      break;
+    default:
+      icon = <FaGlobe />;
+      break;
+  }
+  return (
+    <a href={href} target="_blank" rel="noreferrer" className={styles.button}>
+      {icon}
+      <p style={{position: 'absolute', color: 'transparent'}}>{types}</p>
+    </a>
+  );
+};
 
-export default CircleButton
+export default CircleButton;
